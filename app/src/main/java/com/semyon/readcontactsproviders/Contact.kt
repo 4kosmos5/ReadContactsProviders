776268926c86f193d097c0c81fce5fa9f19f1e05
4kosmos5 <s.altushkin@wellsoft.pro>
2024-03-25 20:49:29 +0500

    Initial commit

---
+++ app/src/main/java/com/semyon/readcontactsproviders/Contact.kt
@@ -0,0 +1,6 @@
+package com.semyon.readcontactsproviders
+
+data class Contact(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/semyon/readcontactsproviders/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.semyon.readcontactsproviders
+
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.semyon.readcontactsproviders.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        requestContacts()
+    }
+
+    private fun requestContacts() {
+        thread {
+            val cursor = contentResolver.query(
+                ContactsContract.Contacts.CONTENT_URI,
+                null,
+                null,
+                null,
+                null
+            )
+            while (cursor?.moveToNext() == true) {
+                val id = cursor.getInt(
+                    cursor.getColumnIndexOrThrow(ContactsContract.Contacts._ID)
+                )
+                val name = cursor.getString(
+                    cursor.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME)
+                )
+                val contact = Contact(id, name)
+                Log.d("MainActivity", contact.toString())
+            }
+            cursor?.close()
+        }
+    }
+}
